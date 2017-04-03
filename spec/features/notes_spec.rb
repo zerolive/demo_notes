2c65a609ad81189fa56d313eb57d6b9abc826eb0
zerolive <zerolive@gmail.com>
2017-04-03 23:17:29 +0200

    Update note test

--- spec/features/notes_spec.rb
+++ spec/features/notes_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 require 'capybara'
 
 describe 'Notes' do
+  before(:each) do
+    Note.delete_all
+  end
+
   it 'create notes' do
     visit '/notes/new'
 
@@ -9,5 +13,17 @@ describe 'Notes' do
     click_on('submit')
 
     expect(page).to have_content('test note')
+  end
+
+  it 'update notes' do
+    note = Note.new(text: 'test note')
+    note.save
+
+    visit '/notes/list'
+    click_on('edit')
+    fill_in('note_text', with: 'test note edited')
+    click_on('submit')
+
+    expect(page).to have_content('test note edited')
   end
 end
